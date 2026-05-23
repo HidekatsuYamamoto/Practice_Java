220502fe90e902d18a47bc43af7a9194e5e0b4e4
Hidekatsu Yamamoto <yokan@dk9.so-net.ne.jp>
2026-05-24 00:01:43 +0900

    5/24

--- Chapter.08/No.05/Main.java
+++ Chapter.08/No.05/Main.java
@@ -6,17 +6,32 @@ import java.util.function.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Sample> list = Arrays.asList(
-                new Sample(10),
-                new Sample(20),
-                new Sample(30));
-        // [ 空欄 ]
-        Predicate<Sample> x = s -> list.contains(s);
-        // Supplier<Sample> x = s -> list.contains(s);
-        // Consumer<Sample> x = s -> list.contains(s);
-        // Function<Sample> x = s -> list.contains(s);
-        if (x.test(new Sample(20))) {
-            System.out.println("ok");
+        {
+            List<Sample> list = Arrays.asList(
+                    new Sample(10),
+                    new Sample(20),
+                    new Sample(30));
+            // [ 空欄 ]
+            Predicate<Sample> x = s -> list.contains(s); // ⓵Same as below process
+            // Predicate<Sample> x = (Sample s)-> {
+            // return list.contains(s);
+            // };
+            // Supplier<Sample> x = s -> list.contains(s);
+            // Consumer<Sample> x = s -> list.contains(s);
+            // Function<Sample> x = s -> list.contains(s);
+            if (x.test(new Sample(20))) { // ⓶
+                System.out.println("ok");
+            }
+        }
+        // 以降、問題文以外
+        // Supplier:引数を受け取らずに値を戻す関数型インターフェース
+        {
+            List<Sample> list = Arrays.asList(
+                    new Sample(10),
+                    new Sample(20),
+                    new Sample(30));
+            Supplier<List<Sample>> x = () -> list;
+            System.out.println(x.get());
         }
     }
 }
@@ -47,3 +62,7 @@ class Sample {
 //
 //
 // 解答A.
+
+// Predicateのtestは、引数のデータと、Predicateで指定されたデータを比較する。その結果、boolean値を返す⓶。
+// 具体的な処理は、Listのcontainで、指定の要素がこのリストに含まれている場合にtrueを返すもの⓵。
+//
